89d07f84e2218449e1d62df0b4956ff007d1d7cb
xElon-Mask <xelonmask@protonmail.com>
2022-04-09 15:44:05 +0200

    add function setNombre (used exclusively by the owner)

--- StartPausedDestroySC.sol
+++ StartPausedDestroySC.sol
@@ -6,9 +6,15 @@ pragma solidity 0.8.13;
 contract Owner {
 
     address owner;
+    uint nombre;
 
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function setNombre(uint _nombre) public {
+        require(msg.sender == owner, "Vous n'avez pas les droits d'acces sur ce contrat");
+        nombre = _nombre;
     }
 }
