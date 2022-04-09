0c7fc97bae8840b60a58b03462158fa04c06c033
xElon-Mask <xelonmask@protonmail.com>
2022-04-09 15:50:54 +0200

    add activatePaused function (access only by the owner)

--- StartPausedDestroySC.sol
+++ StartPausedDestroySC.sol
@@ -6,6 +6,7 @@ pragma solidity 0.8.13;
 contract Owner {
 
     address owner;
+    bool paused;
     uint nombre;
 
 
@@ -13,12 +14,19 @@ contract Owner {
         owner = msg.sender;
     }
 
+    function activatePaused(bool _paused) public {
+        require(msg.sender == owner, "Vous n'avez pas les droits d'acces sur ce contrat");
+        paused = _paused;
+    }
+
     function setNombre(uint _nombre) public {
+        require(!paused, "Le contrat est en pause");
         require(msg.sender == owner, "Vous n'avez pas les droits d'acces sur ce contrat");
         nombre = _nombre;
     }
 
     function getNombre() public view returns(uint) {
+        require(!paused, "Le contrat est en pause");
         return nombre;
     }
 }
